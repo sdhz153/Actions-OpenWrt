997b06017382f8f1d41505e3f9e82ce3ee504184
sdhz150 <yanghui150@163.com>
2024-12-19 12:58:44 +0800

    add passwall.config.sh

---
+++ passwall.config.sh
@@ -0,0 +1,28 @@
+#!/bin/bash
+#
+echo 'CONFIG_PACKAGE_luci-app-passwall=y' >>.config
+echo 'CONFIG_PACKAGE_luci-app-passwall2=y' >>.config
+echo 'CONFIG_PACKAGE_luci-app-passwall2_INCLUDE_Haproxy=y' >>.config
+echo 'CONFIG_PACKAGE_luci-app-passwall2_INCLUDE_Simple_Obfs=y' >>.config
+echo 'CONFIG_PACKAGE_luci-app-passwall2_INCLUDE_SingBox=y' >>.config
+echo 'CONFIG_PACKAGE_luci-app-passwall2_INCLUDE_V2ray_Plugin=y' >>.config
+echo 'CONFIG_PACKAGE_luci-app-passwall2_Nftables_Transparent_Proxy=y' >>.config
+echo 'CONFIG_PACKAGE_luci-app-passwall_INCLUDE_Haproxy=y' >>.config
+echo 'CONFIG_PACKAGE_luci-app-passwall_INCLUDE_Simple_Obfs=y' >>.config
+echo 'CONFIG_PACKAGE_luci-app-passwall_INCLUDE_SingBox=y' >>.config
+echo 'CONFIG_PACKAGE_luci-app-passwall_INCLUDE_V2ray_Geodata=y' >>.config
+echo 'CONFIG_PACKAGE_luci-app-passwall_INCLUDE_V2ray_Geoview=y' >>.config
+echo 'CONFIG_PACKAGE_luci-app-passwall_INCLUDE_V2ray_Plugin=y' >>.config
+echo 'CONFIG_PACKAGE_luci-app-passwall_INCLUDE_Xray=y' >>.config
+echo 'CONFIG_PACKAGE_luci-app-passwall_Nftables_Transparent_Proxy=y' >>.config
+echo 'CONFIG_PACKAGE_luci-i18n-passwall-zh-cn=y' >>.config
+echo 'CONFIG_PACKAGE_luci-i18n-passwall2-zh-cn=y' >>.config
+echo 'CONFIG_PACKAGE_shadowsocksr-libev-ssr-local=y' >>.config
+echo 'CONFIG_PACKAGE_shadowsocksr-libev-ssr-redir=y' >>.config
+echo 'CONFIG_PACKAGE_v2dat=y' >>.config
+echo 'CONFIG_PACKAGE_v2ray-geoip=y' >>.config
+echo 'CONFIG_PACKAGE_v2ray-geosite=y' >>.config
+echo 'CONFIG_PACKAGE_v2ray-plugin=y' >>.config
+echo 'CONFIG_PACKAGE_xray-core=y' >>.config
+echo '# CONFIG_PACKAGE_luci-app-passwall_INCLUDE_Trojan_Plus is not set' >>.config
+echo '# CONFIG_PACKAGE_trojan-plus is not set' >>.config
